eafd2ed1ba7f6f46a6e29d8ce07736dd600dfe5e
Akihiro Fujii <aki_insane_liquor@yahoo.co.jp>
2014-01-23 16:09:20 +0900

    できた

--- omikuji/ViewController.h
+++ omikuji/ViewController.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+
+    __weak IBOutlet UIButton *omi1btn;
+
+    __weak IBOutlet UIButton *omi2btn;
+    
+    __weak IBOutlet UIButton *omi3btn;
+    
+    __weak IBOutlet UIButton *againbtn;
+    
+    
+    __weak IBOutlet UILabel *kekkalbl;
+    
+    __weak IBOutlet UIImageView *gazouImg;
+    
+    
+    __weak IBOutlet UILabel *meslbl;
+    
+    
+}
+
+-(IBAction) omi1btn_down:(id)sender;
+-(IBAction) omi2btn_down:(id)sender;
+-(IBAction) omi3btn_down:(id)sender;
+-(IBAction) againbtn_down:(id)sender;
+
+-(void)image;
+
 
 @end
--- omikuji/ViewController.m
+++ omikuji/ViewController.m
@@ -8,15 +8,36 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    int random;
+}
+
 
 @end
 
 @implementation ViewController
 
+UIImage *omi1Image;
+UIImage *omi2Image;
+UIImage *omi3Image;
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    kekkalbl.hidden = YES;
+    againbtn.hidden = YES;
+    
+    omi1Image = [UIImage imageNamed:@"gu.png"];
+    omi2Image = [UIImage imageNamed:@"ch.png"];
+    omi3Image = [UIImage imageNamed:@"pa.png"];
+    
+    [kekkalbl setFont:[UIFont boldSystemFontOfSize:18.0f]];
+    [kekkalbl setTextColor:[UIColor blackColor]];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +46,91 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(IBAction) omi1btn_down:(id)sender{
+    meslbl.text = @"結果は...";
+    omi1btn.hidden = YES;
+    omi2btn.hidden = YES;
+    omi3btn.hidden = YES;
+    gazouImg.hidden = NO;
+    kekkalbl.hidden = NO;
+    againbtn.hidden = NO;
+    
+    [self image];
+}
+-(IBAction) omi2btn_down:(id)sender{
+    meslbl.text = @"結果は...";
+    omi1btn.hidden = YES;
+    omi2btn.hidden = YES;
+    omi3btn.hidden = YES;
+    gazouImg.hidden = NO;
+    kekkalbl.hidden = NO;
+    againbtn.hidden = NO;
+    
+    [self image];
+}
+-(IBAction) omi3btn_down:(id)sender{
+    meslbl.text = @"結果は...";
+    omi1btn.hidden = YES;
+    omi2btn.hidden = YES;
+    omi3btn.hidden = YES;
+    gazouImg.hidden = NO;
+    kekkalbl.hidden = NO;
+    againbtn.hidden = NO;
+    
+    [self image];
+}
+-(IBAction) againbtn_down:(id)sender{
+    
+    omi1btn.hidden = NO;
+    omi2btn.hidden = NO;
+    omi3btn.hidden = NO;
+    meslbl.text = @"結果は...";
+    [gazouImg setImage:nil];
+    kekkalbl.text = @"";
+    againbtn.hidden = YES;
+}
+
+- (void)image{
+    random = arc4random() % 6;
+    
+    switch (random) {
+        case 0:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_daikichi"];
+            kekkalbl.text = @"とってもいいことがあります！";
+            break;
+            
+        case 1:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_chuukichi"];
+            kekkalbl.text = @"そこそこいいことがあるかもです！";
+            break;
+        case 2:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_suekichi"];
+            kekkalbl.text = @"ぼちぼちいいことがあるです！";
+            break;
+        case 3:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_syoukichi"];
+            kekkalbl.text = @"いいことがあるような気がします！";
+            break;
+        case 4:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_kyou"];
+            kekkalbl.text = @"気をつけてください！";
+            break;
+        case 5:
+            gazouImg.image = [UIImage imageNamed:@"omikuji_daikyou"];
+            kekkalbl.text = @"DANGER";
+            
+            
+            break;
+            
+        default:
+            break;
+    }
+    
+}
+
+
+
+
 
 @end
